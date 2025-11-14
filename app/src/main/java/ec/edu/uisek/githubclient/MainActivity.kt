30c4bc93e7d15bf5564cc0ed6c7f2fa9fcfc4dcc
gerente90 <jeffersonlayer1990@gmail.com>
2025-11-14 15:10:56 -0500

    Avance

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,10 +1,12 @@
 package ec.edu.uisek.githubclient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ec.edu.uisek.githubclient.services.RetrofitClient
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -26,4 +28,39 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun fetchRepositories() {
+        val apiService = RetrofitClient.gitHubApiService
+        val call = apiService.getRepos()
+
+        call.enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                TODO("Not yet implemented")
+                if (response.isSuccessful) {
+                    val repos = response.body()
+                    if (repos != null && repos.isNotEmpty()) {
+                        reposAdapter.updateRepositories(repos)
+                    } else {
+                        showMessage("No repositories found.")
+                    }
+                }
+                else {
+                    val errorMsg = when (response.code()) {
+                        401 -> "Unauthorized"
+                        403 -> "Forbidden"
+                        404 -> "Not Found"
+                        else -> "Unknown Error"
+                    }
+                    Log.e("MainActivity", "API Error: $errorMsg")
+                    showMessage("Error: $errorMsg")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                showMessage("Error de conexión")
+                Log.e("MainActivity", "API Error: ${t.message}")
+
+            }
+        })
+
+    }
 }
